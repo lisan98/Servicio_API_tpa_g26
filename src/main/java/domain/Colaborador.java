70358461dd065d64d844fbe0cee7287ea3056233
lisan98 <lisandro0@hotmail.com>
2024-09-08 16:33:44 -0300

    agregue GeneradorSeleccion

---
+++ src/main/java/domain/Colaborador.java
@@ -0,0 +1,9 @@
+package domain;
+
+public class Colaborador {
+    private String nombre;
+    private String contacto;
+    private int puntaje;
+    private int colabRealizadas;
+
+}
---
+++ src/main/java/domain/GeneradorSeleccion.java
@@ -0,0 +1,11 @@
+package domain;
+
+import java.util.List;
+
+public class GeneradorSeleccion {
+    private List<SolicitudRecomendacion> solicitudes;
+    private List<Colaborador> colaboradores;
+    public List<Colaborador> generarSeleccion(){
+        List<Colaborador> colaboradoresPosibles;
+    }
+}
