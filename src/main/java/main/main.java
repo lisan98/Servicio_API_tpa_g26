9d4169ce3acaec9e07b11931d70850304a9db19e
lisan98 <lisandro0@hotmail.com>
2024-10-13 16:35:11 -0300

    agregue api para carga de colaboradores

--- src/main/java/main/main.java
+++ src/main/java/main/main.java
@@ -1,10 +1,13 @@
 package main;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import domain.Colaborador;
 import domain.SolicitudRecomendacion;
 import io.javalin.Javalin;
 import repositories.ColaboradorRepository;
 import repositories.SolicitudRepository;
+
 
 import java.util.List;
 
@@ -14,7 +17,7 @@ public class main {
         SolicitudRepository solirepo = new SolicitudRepository();
         ColaboradorRepository colarepo = new ColaboradorRepository();
 
-        app.get("api/colaboradores", ctx -> {
+        app.get("api/recomendar", ctx -> {
             int puntosMinimos = Integer.parseInt(ctx.body());
             int donacionesMinimas = Integer.parseInt(ctx.body());
             int maxColaboradores = Integer.parseInt(ctx.body());
@@ -22,9 +25,21 @@ public class main {
             SolicitudRecomendacion solicitud = solirepo.crearSolicitud(puntosMinimos, donacionesMinimas, maxColaboradores);
             List<Colaborador> colaboradores =solirepo.obtenerColaboradores(puntosMinimos, donacionesMinimas, maxColaboradores);
             solicitud.setColaboradores(colaboradores);
-            colarepo.addColaborador(colaborador);
+       //     colarepo.addColaborador(colaborador);
             /*tengo que agregar un metodo para agregar una lista al repo*/
             ctx.json(colaboradores);
+        });
+
+        app.post("/api/colaboradores", ctx -> {
+            List<Colaborador> colaboradores = objectMapper.readValue(
+                    ctx.body(),
+                    new TypeReference<List<Colaborador>>() {}
+            );
+
+            // Guardar colaboradores en la base de datos
+            colaboradorService.guardarColaboradores(colaboradores);
+
+            ctx.status(201).result("Colaboradores almacenados correctamente.");
         });
     }
 }
